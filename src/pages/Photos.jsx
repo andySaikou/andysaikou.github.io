7f8bd3ede5cb8693cfbba4522f2ec2b9c08281b4
andySaikou <andysaikou2526@gmail.com>
2025-10-08 00:08:38 +0100

    Add more photos to the photo page, modify page format

--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -3,8 +3,20 @@ import './Photos.css';
 export default function Photos() {
     const photos = [
         [
+            { src: "/photos/DSCF0238.webp", caption: "Mariaberget, Stockholm" },
+            { src: "/photos/DSCF1030.webp", caption: "Mariaberget, Stockholm" },
+        ],
+        [
             { src: "/photos/DSCF1170.webp", caption: "Gamla Stan, Stockholm" },
             { src: "/photos/DSCF0687.webp", caption: "Paviljongsplan, Visby" },
+        ],
+        [
+            { src: "/photos/DSCF0643.webp", caption: "Town center, Visby" },
+            { src: "/photos/DSCF0644.webp", caption: "Town center, Visby" },
+        ],
+        [
+            { src: "/photos/lunar-eclipse.webp", caption: "Lunar Eclipse, Sept 2025" },
+            { src: "/photos/DSCF0109.webp", caption: "Hastings, East Sussex" },
         ],
         [
             { src: "/photos/DSCF0012.webp", caption: "South Haikou, Haihan" },
@@ -12,12 +24,30 @@ export default function Photos() {
             { src: "/photos/DSCF0320.webp", caption: "Saltburn, North Yorkshire" },
         ],
         [
+            { src: "/photos/DSCF0126.webp", caption: "Greenwich, London" },
+            { src: "/photos/DSCF0040.webp", caption: "Takaya Shrine, Kan-onji" },
+        ],
+        [
+            { src: "/photos/DSCF0068.webp", caption: "Seto Inland Sea, Takamatsu" },
+            { src: "/photos/DSCF0294.webp", caption: "Kawaramachi, Kyoto" },
+        ],
+        [
+            { src: "/photos/0017_14A.webp", caption: "Osaka Station, Osaka (film)" },
+            { src: "/photos/0036_33A.webp", caption: "Umeda, Osaka (film)" },
+            { src: "/photos/DSCF0127.webp", caption: "Saiko-ji Temple, Shodoshima" },
+        ],
+        [
+            { src: "/photos/000055.webp", caption: "鼓楼 Gu Lou, Beijing (film)" },
+            { src: "/photos/000045.webp", caption: "后海 Hou Hai, Beijing (film)" },
+            { src: "/photos/000048.webp", caption: "后海 Hou Hai, Beijing (film)" },
+        ],
+        [
             { src: "/photos/DSCF0102.webp", caption: "Soho, London" },
             { src: "/photos/DSCF0254.webp", caption: "South Kensington, London" },
             { src: "/photos/DSCF0538.webp", caption: "London Bridge, London" },
         ],
         [
-            { src: "/photos/DSCF0110.webp", caption: "Dover, London" },
+            { src: "/photos/DSCF0110.webp", caption: "Dover, Kent" },
             { src: "/photos/DSCF0031.webp", caption: "Tower Pier, London" },
         ],
     ];
